e124ee06dd5d0077429cd7ceb2b5b08d20ea5024
Florian Brunner <if20b186@technikum-wien.at>
2020-11-13 21:11:03 +0100

    Refactoring and adding StatusCode functionality

---
+++ Webserver/Webserver/Messages.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Webserver
+{
+    public class Messages
+    {
+        string[] messages = new string[1000];
+        static int counter = 0;
+        public string GetMessages()
+        {
+            string res = "";
+            foreach (var item in messages)
+            {
+                res+=item+"\n";
+            }
+            return res;
+        }
+        public int PostMessage(string message)
+        {
+            messages[counter]=message;
+            counter++;
+            return counter;
+        }
+        public string GetMessage(int id)
+        {
+            if (id > counter&&id-1<0)
+                return null;
+            return messages[id-1];
+        }
+
+        public bool UpdateMessage(int id, string message)
+        {
+            Console.WriteLine(id +" "+message);
+            if (id - 1 < 0)
+                return false;
+            messages[id-1] = message;
+            return true;
+        }
+        public bool DeleteMessage(int id)
+        {
+            if (id - 1 < 0)
+                return false;
+            messages[id-1]=null;
+            counter--;
+            return true;
+        }
+    }
+}
---
+++ Webserver/Webserver/RequestContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Webserver
+{
+    public class RequestContext
+    {
+        public string Http_verb { get; }
+        public string Path { get; }
+        public string Http_version { get; }
+        public Dictionary<string, string> Header { get; } = new Dictionary<string, string>();
+        public string Payload { get; }
+        public int Requestedfield { get; }
+
+        public RequestContext(string verb, string path, string http_version, Dictionary<string,string> header, string payload,int requestedfield)
+        {
+            this.Http_verb = verb;
+            this.Path = path;
+            this.Http_version = http_version;
+            this.Header = header;
+            this.Payload = payload;
+            this.Requestedfield = requestedfield;
+        }
+    }
+}
