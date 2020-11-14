886a6425717957d983ae92d0bb495e3eaf9b734d
Florian Brunner <if20b186@technikum-wien.at>
2020-11-14 23:40:28 +0100

    Adding first mock

---
+++ Webserver/Webserver/MyTcpClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Webserver
+{
+    class MyTcpClient : ITcpClient
+    {
+        private TcpClient _tcpClient;
+        public MyTcpClient(TcpClient client)
+        {
+            _tcpClient = client;
+        }
+        public void Close()=>
+            _tcpClient.Close();
+
+        public Stream GetReadStream()=>
+            _tcpClient.GetStream();
+
+        public Stream GetWriteStream() =>
+            _tcpClient.GetStream();
+    }
+}
